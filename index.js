372043d17229f36fde211ec9fe08e8f9eadf1621
DeanNguyen98 <nctuan.hn@gmail.com>
2024-09-20 23:15:28 +1000

    simple routing for page

---
+++ index.js
@@ -0,0 +1,28 @@
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    let filePath = path.join(__dirname, 'views', req.url === '/' ? 'index.html' : req.url);
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            if (err.code == 'ENOENT') {
+                fs.readFile(path.join(__dirname, 'views', '404.html'), (err, content) => {
+                    res.writeHead(200, {'Content-Type' : 'text/html'});
+                    res.end(content, 'utf8');
+                })
+            } else {
+                res.writeHead(500);
+                res.end(`Server Error : ${err.code}`);
+            }
+        } else {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(content, 'utf8');
+        }
+    })
+})
+
+const PORT = process.env.PORT || 8080;
+server.listen(PORT, () => {
+    console.log(`Server running on port: ${PORT}`);
+})
